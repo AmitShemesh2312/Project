faa2c404690dfaaf62307be2bbe764e2cf4c88cb
Owner <idanhazon1@gmail.com>
2022-11-16 09:23:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void createRoom(View view) {
+        Intent intent = new Intent(this, createRoom());
+        startActivity(intent);
+    }
+
+    public void JoinRoom(View view) {
+
+    }
+}
